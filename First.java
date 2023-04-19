9d2643d11540d9b55c8feca24aac6ed2908aed6b
mohitjangra2196 <mohitoffical2196@gmail.com>
2023-04-19 09:50:25 -0700

    edit new hello

--- First.java
+++ First.java
@@ -2,7 +2,8 @@ package JAVA;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("helllo world");
+        System.out.println("helllo world ");  
+        System.out.println("hello git");
     }
     
 }
